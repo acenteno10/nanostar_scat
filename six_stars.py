9eb836880d0de30f3c732f69d5e058fb476942cb
acenteno10 <68850381+acenteno10@users.noreply.github.com>
2025-03-06 12:31:25 +0000

    Add files via upload

---
+++ six_stars.py
@@ -0,0 +1,23 @@
+from core_star import *
+
+'''
+Purpose:
+    To model a nano star with a dielectric core optical properties of gold nanostars.
+    This code writes the shape.dat file for ddscat code. 
+    
+
+
+'''
+
+#x = np.linspace(0.01,0.3,10) # Gaussian Variance from 1-30% of the average tip
+
+R = 16 #radius of core
+tipR = 10 #radius of cone 
+tipH = 20 #height of cone
+N=0.6666 #the dipole spacing in nm
+T=4 #thickness of shell
+pos= nanostar_six(R,tipR,tipH,N,T)
+print ('shape.dat written')
+print (' Edit ddscat.par for wavelengths and dielectric material')
+print ('Then you can run ddscat to obtain the extinction, absorption and scattering efficiencies for the nanostar')
+print ('DDSCAT places the results in qtable')
